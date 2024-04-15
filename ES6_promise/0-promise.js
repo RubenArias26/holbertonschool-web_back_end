99abbb741684d126da6cc6c6fdba6db258e77927
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-04-14 22:24:08 -0500

    I added a new dir, call ES6 promise with two new files

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,31 @@
+// this function returns a promise
+// Esta funcion devuelve una promesa
+
+export default function getResponseFromAPI() {
+  // se crea una nueva promesa
+  return new Promise((resolve, reject) => {
+    // se simula una llamada al API
+    // Devulve un resultado en un tiempo determinado
+    setTimeout(() => {
+      const exito = true;
+      // Se obtiene la respuesta del API en un json
+
+      if (exito) {
+        resolve('');
+      } else {
+        reject(new Error('Error al obtener los datos'));
+      }
+    }, 2000);
+  });
+}
+
+// Manejamos la promesa con .then para el exito
+getResponseFromAPI()
+  .then((data) => {
+    console.log(data);
+  })
+
+  // Utilizamos el catch para el error
+  .catch((error) => {
+    console.error('Los datos no fueron obtenidos:', error.message);
+  });
